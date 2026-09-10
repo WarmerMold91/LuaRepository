73e90b26d8164fba423f4f0d9160964f9a20b348
WarmerMold91 <warmermold91@gmail.com>
2026-09-10 13:02:58 -0500

    LuaRepository

--- Main.lua
+++ Main.lua
@@ -1,2 +1,26 @@
+--[[
+Lua Practice
+09/10/2026
+]]
+
+--lua54 (File Name)
+
 print("this is my first lua code.")
 print("I am updating my code!")
+
+math.randomseed(os.time())
+
+-- this is a comment(Single loadstring
+
+--[[
+this
+is a multi line
+comment.
+]]
+
+print("what is you name?")
+
+name = io.read()
+
+print(name)
+
